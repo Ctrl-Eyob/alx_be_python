e86e8b01c7dbc18c15620d5023adf37a1352f910
Ctrl-Eyob <eyobaberabekru@gmail.com>
2025-11-27 16:35:25 +0300

    implement basic library management system with book and library classes

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(100, 200), 300)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 10), -10)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(3, 2), 1.5)
+
+        # Division by zero
+        self.assertIsNone(self.calc.divide(10, 0))
+
+if __name__ == '__main__':
+    unittest.main()
